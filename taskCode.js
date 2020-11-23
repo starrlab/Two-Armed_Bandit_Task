2d40a457ffc4563843e20ae9d5785ae67e4f4c63
unknown <mrrandyperrone@gmail.com>
2020-11-23 13:53:31 -0800

    Shows feedback winner and loser.

--- taskCode.js
+++ taskCode.js
@@ -33,10 +33,10 @@ let decide = {
     type: "html-keyboard-response",
     choices: jsPsych.NO_KEYS,
     trial_duration: DECIDE_DURATION,
-    stimulus: "<h1>Decide Which Lever to Pull!</h1>" +
-        "<div class='container'>"+
+    stimulus: "<div class='container'>"+
         "<div  '><img src='img/HandleLeft.png'></img>" +
         "<p class='small'><strong>Press the ← key</strong></p></div>" +
+        "<div  '><h1>Decide Lever to Pull!</h1></div>" +
         "<div  '><img src='img/HandleRight.png'></img>" +
         "<p class='small'><strong>Press the → key</strong></p></div>" +
         "</div>",
@@ -49,10 +49,10 @@ timeline.push(decide);
 let action = {
     type: "html-keyboard-response",
     choices: [KEYBOARD_PRESS_RIGHT, KEYBOARD_PRESS_LEFT],
-    stimulus: "<h1>Pull the Lever!</h1>" +
-        "<div class='container'>"+
+    stimulus: "<div class='container'>"+
         "<div  '><img src='img/HandleLeft.png'></img>" +
         "<p class='small'><strong>Press the ← key</strong></p></div>" +
+        "<div  '><h1>Pull a Lever!</h1></div>" +
         "<div  '><img src='img/HandleRight.png'></img>" +
         "<p class='small'><strong>Press the → key</strong></p></div>" +
         "</div>",
@@ -61,6 +61,40 @@ let action = {
 };
 //add this to timeline
 timeline.push(action);
+
+let feedbackWinner = {
+    type: "html-keyboard-response",
+    choices: jsPsych.NO_KEYS,
+    trial_duration: DECIDE_DURATION,
+    stimulus: "<div class='container'>"+
+        "<div  '><img src='img/HandleLeft.png'></img>" +
+        "<p class='small'><strong>Press the ← key</strong></p></div>" +
+        "<div  '><h1 id='checkmark_for_winner'>✓</h1></div>" +
+        "<div  '><img src='img/HandleRight.png'></img>" +
+        "<p class='small'><strong>Press the → key</strong></p></div>" +
+        "</div>",
+    on_finish: function (data) {
+    }
+};
+//add this to timeline
+timeline.push(feedbackWinner);
+
+let feedbackLoser = {
+    type: "html-keyboard-response",
+    choices: jsPsych.NO_KEYS,
+    trial_duration: DECIDE_DURATION,
+    stimulus: "<div class='container'>"+
+        "<div  '><img src='img/HandleLeft.png'></img>" +
+        "<p class='small'><strong>Press the ← key</strong></p></div>" +
+        "<div  '><h1 id='x_for_loser'>X</h1></div>" +
+        "<div  '><img src='img/HandleRight.png'></img>" +
+        "<p class='small'><strong>Press the → key</strong></p></div>" +
+        "</div>",
+    on_finish: function (data) {
+    }
+};
+//add this to timeline
+timeline.push(feedbackLoser);
 
 /*********Start Experiment************/
 //Display data shows the data displayed at end of trials
