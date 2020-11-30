ebc1d22bb5b34d7b61be73306b06b0dd3677ce65
unknown <mrrandyperrone@gmail.com>
2020-11-30 14:41:19 -0800

    Putting together the way the win and loss will work. It will choose based on correct response of lever choice and display the correct win or loss page

--- taskCode.js
+++ taskCode.js
@@ -14,28 +14,11 @@ let currentBlockNumber = 1;
 let currentTrialNumber = 1;
 let timeline = [];
 let levers = ["leftLever", "rightLever"];
+let leftArrowPressed = true;
 
 let probability_start_left = [.8, .8, .8, .8, .2, .2, .2, .2];
 shuffleArray(probability_start_left);
 //let probs = [.2, .8];
-
-
-
-/*
-const STIMULUS_DURATION = 2000; //This is the total time the image will be displayed before disapearing.
-const TRIAL_DURATION = 3000; //This is the total time before the curent trial moves on to next trial
-const POST_TRIAL_GAP = [1000, 1250, 1500, 1750, 2000]; //Sets the time after the trial finishes to wait until the fixation starts (trial hang time).
-
-//Image settings
-const STIMULUS_HEIGHT = 550; //Changes the height of the images. Set to null for no changes
-const STIMULUS_WIDTH = null; //Changes the width of the images.  Set to null for no changes
-const MAINTAIN_IMG_ASPECT_RATIO = true; //must be true or false. Set only the width or height and set to true will keep the aspect ration of the image. Set to false if want to change height/width together.
-
-//Fixation settings
-const FIXATION_DURATION = [1000]; //Sets the fixation duration. Can add as many values as you want or subtract values from array.
-const FIXATION_KEY = '+';
-const FIXATION_SIZE = 60;
- */
 
 let decide = {
     type: "html-keyboard-response",
@@ -60,7 +43,14 @@ let action = {
         "<div  '><img src='img/HandleRight.png'></img></div>" +
         "</div>",
     on_finish: function (data) {
-
+        console.log(data.key_press);
+        console.log(KEYBOARD_PRESS_RIGHT);
+        if(data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(KEYBOARD_PRESS_RIGHT)){
+            leftArrowPressed = false;
+        }
+        else{
+            leftArrowPressed = true;
+        }
     }
 };
 
@@ -77,7 +67,9 @@ let feedbackWinner = {
 
     },
     on_load: function (data) {
-       // jsPsych.finishTrial();
+        if(leftArrowPressed){
+            jsPsych.finishTrial();
+        }
     }
 };
 
@@ -94,7 +86,9 @@ let feedbackLoser = {
 
     },
     on_load: function (data) {
-        //jsPsych.finishTrial();
+        if(!leftArrowPressed){
+            jsPsych.finishTrial();
+        }
     }
 };
 
