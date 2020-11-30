0c5cf75453025e1889ab4c6ff05130fa53bc1789
unknown <mrrandyperrone@gmail.com>
2020-11-30 10:48:27 -0800

    Removed the arrows under the lever

--- taskCode.js
+++ taskCode.js
@@ -34,11 +34,9 @@ let decide = {
     choices: jsPsych.NO_KEYS,
     trial_duration: DECIDE_DURATION,
     stimulus: "<div class='container'>"+
-        "<div  '><img src='img/HandleLeft.png'></img>" +
-        "<p class='small'><strong>Press the ← key</strong></p></div>" +
+        "<div  '><img src='img/HandleLeft.png'></img></div>" +
         "<div  '><h1>Decide Lever to Pull!</h1></div>" +
-        "<div  '><img src='img/HandleRight.png'></img>" +
-        "<p class='small'><strong>Press the → key</strong></p></div>" +
+        "<div  '><img src='img/HandleRight.png'></img></div>" +
         "</div>",
     on_finish: function (data) {
     }
@@ -50,11 +48,9 @@ let action = {
     type: "html-keyboard-response",
     choices: [KEYBOARD_PRESS_RIGHT, KEYBOARD_PRESS_LEFT],
     stimulus: "<div class='container'>"+
-        "<div  '><img src='img/HandleLeft.png'></img>" +
-        "<p class='small'><strong>Press the ← key</strong></p></div>" +
+        "<div  '><img src='img/HandleLeft.png'></img></div>" +
         "<div  '><h1>Pull a Lever!</h1></div>" +
-        "<div  '><img src='img/HandleRight.png'></img>" +
-        "<p class='small'><strong>Press the → key</strong></p></div>" +
+        "<div  '><img src='img/HandleRight.png'></img></div>" +
         "</div>",
     on_finish: function (data) {
     }
@@ -67,11 +63,9 @@ let feedbackWinner = {
     choices: jsPsych.NO_KEYS,
     trial_duration: DECIDE_DURATION,
     stimulus: "<div class='container'>"+
-        "<div  '><img src='img/HandleLeft.png'></img>" +
-        "<p class='small'><strong>Press the ← key</strong></p></div>" +
+        "<div  '><img src='img/HandleLeft.png'></img></div>" +
         "<div  '><h1 id='checkmark_for_winner'>✓</h1></div>" +
-        "<div  '><img src='img/HandleRight.png'></img>" +
-        "<p class='small'><strong>Press the → key</strong></p></div>" +
+        "<div  '><img src='img/HandleRight.png'></img></div>" +
         "</div>",
     on_finish: function (data) {
     }
@@ -84,11 +78,9 @@ let feedbackLoser = {
     choices: jsPsych.NO_KEYS,
     trial_duration: DECIDE_DURATION,
     stimulus: "<div class='container'>"+
-        "<div  '><img src='img/HandleLeft.png'></img>" +
-        "<p class='small'><strong>Press the ← key</strong></p></div>" +
+        "<div  '><img src='img/HandleLeft.png'></img></div>" +
         "<div  '><h1 id='x_for_loser'>X</h1></div>" +
-        "<div  '><img src='img/HandleRight.png'></img>" +
-        "<p class='small'><strong>Press the → key</strong></p></div>" +
+        "<div  '><img src='img/HandleRight.png'></img></div>" +
         "</div>",
     on_finish: function (data) {
     }
