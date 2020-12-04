063a09ed883bb2dc06b1271431e1bc9506adf9c9
unknown <mrrandyperrone@gmail.com>
2020-12-04 14:14:58 -0800

    Saves the data when finished

--- taskCode.js
+++ taskCode.js
@@ -183,6 +183,8 @@ jsPsych.init({
     timeline: [trialBlocks],
     on_finish: function() {
         jsPsych.data.displayData();
+        let filename = "task_" + Date.now().toString() + "_ver" + VERSION + ".csv";
+        saveData(csvData, filename);
     }
 });
 
@@ -219,3 +221,18 @@ function generateGaussian(mean,std){
     let z0 = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(Math.PI * 2 * u2);
     return z0 * std + mean;
 }
+
+let saveData = (function () {
+    let a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    return function (data, fileName) {
+        let json = data,
+            blob = new Blob([json], { type: "octet/stream" }),
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+}());
